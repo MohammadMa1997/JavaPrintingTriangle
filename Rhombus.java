fb68d5fb3208afa6a2f96819d1c5167b0af63b35
veeshnrb <veeshnrb@yahoo.com>
2014-06-06 12:49:17 -0700

    Java program

---
+++ Rhombus.java
@@ -0,0 +1,30 @@
+
+public class Rhombus {
+
+	
+	public static void main(String[] args) {
+		
+		for(int i=1;i<10;i++)
+		{
+			for(int j=9;j>1;j--){
+				if(i<j){
+					System.out.print(i);
+				
+			}}
+			
+			System.out.println();
+		}
+		
+		for(int i=1;i<10;i++)
+		{
+			if(i<5)
+			
+			{
+				System.out.print(" ");
+				i++;}
+			System.out.println(i);
+		}
+			
+	}
+
+}
